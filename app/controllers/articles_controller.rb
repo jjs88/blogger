8234d0ce1d5261e9eaa8916d1df4e23794525910
jjs88 <jjs457@gmail.com>
2014-01-21 22:38:53 -0500

    added feaures

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,50 @@
 class ArticlesController < ApplicationController
 
+	
+	def new
+		@article = Article.new
+	end
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to article_path(@article)
+	end
+
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update_attributes(article_params)
+
+		flash.notice = "article '#{@article.title}' updated!"
+
+		redirect_to article_path(@article)
+	end
+
+
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
+	private 
+	def article_params
+		params.require(:article).permit(:title, :body)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blogger::Application.routes.draw do
 resources :articles
 
+root to: 'articles#index'
+
 
 end
