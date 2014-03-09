5aaedc38307f2a1c089069a3c55ed87a2798cdef
jjs88 <jjs457@gmail.com>
2014-03-09 00:47:22 -0500

    added tagging

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,12 @@
+class TagsController < ApplicationController
+  
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+  	@tag = Tag.find(params[:id])
+  	@tag.destroy
+  	redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Blogger::Application.routes.draw do
+  get "tags/show"
 resources :articles do 
 	resources :comments
 end
+resources :tags
 
 root to: 'articles#index'
 
